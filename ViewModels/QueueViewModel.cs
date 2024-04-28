78dca6be0df8b08ee6fb4f689601366239ae0cda
Craig M <craigmcmillan@live.co.uk>
2024-04-28 21:00:24 +0100

    updated PlayQueue xaml page and created QueueViewModel

---
+++ ViewModels/QueueViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+
+public class QueueViewModel : INotifyPropertyChanged
+{
+    private Song currentSong;
+
+    public ObservableCollection<Song> Songs { get; set; }
+    public Song CurrentSong
+    {
+        get => currentSong;
+        set { currentSong = value; OnPropertyChanged(); }
+    }
+
+    public QueueViewModel()
+    {
+        Songs = new ObservableCollection<Song>();
+        LoadSongs();
+        SubscribeToChanges();
+    }
+
+    private void SubscribeToChanges()
+    {
+        // Assuming Player is accessible somehow, subscribe to its TrackChanged event
+        Player.TrackChanged += Player_TrackChanged;
+    }
+
+    private void Player_TrackChanged(object sender, EventArgs e)
+    {
+        CurrentSong = Player.GetCurrentTrack(); // Ensure you have a method to get the current track as a Song object
+    }
+
+}
